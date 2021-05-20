23701e57dd6c77963398f76d886351bb2ad10ee3
LucianeVenturini <lucianeventurini@gmail.com>
2021-05-20 11:40:43 +0100

    API connection

--- Cambios/Form1.cs
+++ Cambios/Form1.cs
@@ -1,21 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
+﻿
 namespace Cambios
 {
+    using Modelos;
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Windows.Forms;
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            LoadRates();//para criar as taxas
         }
 
+        private async void LoadRates()
+        {
+            //bool load;//foi carregado ou não
+
+            //para carregar a API via conexão
+            var client = new HttpClient(); //crio http para fazer ligação externa
+            client.BaseAddress = new Uri("http://cambios.somee.com"); //por o endereço
+                                                                      //base da API que podemos ir buscar ao postman
+
+            //controlador da API - uma pasta onde tenho os rates
+            //tarefa assincrona, por isto o método vai passar a ser private async para
+            //aplicação não deixe de estar a correr qdo carrega as taxas  
+            //vamos guardar o resultado numa variavel
+            var response = await client.GetAsync("/api/Rates");
+
+            //variável objecto que vai ficar a espera da resposta que vem de cima
+            //carrego os resultados no formato de uma string pra dentro do objeto result
+            var result = await response.Content.ReadAsStringAsync(); //conteudo lido como uma string
+
+            //para o caso de alguma coisa correr mal
+            if (!response.IsSuccessStatusCode)
+            {
+                MessageBox.Show(response.ReasonPhrase); //tras o erro que se passou
+                return; //se correr mal, sai
+            }
+
+            //se correu tudo bem
+            var rates = JsonConvert.DeserializeObject<List<Rate>>(result);
+
+            cb_origem.DataSource = rates; //lista rates
+
+        }
     }
 }
