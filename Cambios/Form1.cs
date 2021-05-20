023e9a54fd7a326c0f7e5c58d37b556ea6b304f6
LucianeVenturini <lucianeventurini@gmail.com>
2021-05-20 12:09:24 +0100

    Add ApiService

--- Cambios/Form1.cs
+++ Cambios/Form1.cs
@@ -20,6 +20,8 @@ namespace Cambios
         {
             //bool load;//foi carregado ou não
 
+            ProgressBar1.Value = 0;
+
             //para carregar a API via conexão
             var client = new HttpClient(); //crio http para fazer ligação externa
             client.BaseAddress = new Uri("http://cambios.somee.com"); //por o endereço
@@ -45,8 +47,23 @@ namespace Cambios
             //se correu tudo bem
             var rates = JsonConvert.DeserializeObject<List<Rate>>(result);
 
-            cb_origem.DataSource = rates; //lista rates
 
+            cb_origem.DataSource = rates; //lista rates
+            //para aparecer os nomes adequadamente vamos a class rates
+            //criar um override ou então podemos fazer da seguinte forma:
+            cb_origem.DisplayMember = "Name";
+
+            //corrige bug da microsoft
+            cb_destino.BindingContext = new BindingContext(); //limpo a ligação de uma para a outra
+
+            //agora vamos fazer para a cb_destino
+            cb_destino.DataSource = rates;
+            cb_destino.DisplayMember = "Name";
+
+            //desta forma a origem e destino vão apresentar a mesma moeda. 
+            //Para resolver isto vamos fazer um cb_destino.BindingContext na linha 56
+
+            ProgressBar1.Value = 100;
         }
     }
 }
